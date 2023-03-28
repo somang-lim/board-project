a1d47057907beb2718684f7b02e9ac77a96e3bcd
somang-lim <somangjuha@gmail.com>
2023-03-28 15:37:37 +0900

    refactoring : AuditingFields 클래스를 추상 클래스로 변경
    엔티티에서 상속을 받아 사용해야 하는 목적에 맞도록
    `abstract` 키워드를 추가했다.

--- src/main/java/com/fastcampus/boardproject/domain/AuditingFields.java
+++ src/main/java/com/fastcampus/boardproject/domain/AuditingFields.java
@@ -19,7 +19,7 @@ import lombok.ToString;
 @ToString
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
-public class AuditingFields {
+public abstract class AuditingFields {
 
 	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
 	@CreatedDate
