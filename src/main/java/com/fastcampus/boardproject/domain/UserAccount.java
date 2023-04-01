1e0e190c2a93cdc4e9bdae5ddc4d97c300239e37
somang-lim <somangjuha@gmail.com>
2023-04-01 13:36:52 +0900

    #30 - 도메인 업데이트 : UserAccount 회원 ID 에 유니크 키 추가
    회원 id 로 로그인하고 유저를 식별하므로, 당연히 uk 여야 한다.
    이 부분이 설계에서 반영되지 않은 것을 발견했다.
    테스트는 uk 적용으로 기존 `data.sql` 의 테스트 데이터와 
    중복이 발생하지는 않지만 새로운 `userId` 가 추가되는 것을 나타내기 위해
    `userId` 이름을 수정했다.

--- src/main/java/com/fastcampus/boardproject/domain/UserAccount.java
+++ src/main/java/com/fastcampus/boardproject/domain/UserAccount.java
@@ -16,7 +16,7 @@ import lombok.ToString;
 @Getter
 @ToString
 @Table(indexes = {
-	@Index(columnList = "userId"),
+	@Index(columnList = "userId", unique = true),
 	@Index(columnList = "email", unique = true),
 	@Index(columnList = "createdAt"),
 	@Index(columnList = "createdBy")
