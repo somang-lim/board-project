c6a7cf775fc7e12c2375ac948681f52b0f4e9118
somang-lim <somangjuha@gmail.com>
2023-04-02 18:00:45 +0900

    #35 - 해시태그 검색 페이지를 위한 기능 구현
    해시태그 뷰에 모든 게시글에서 나오는 해시태그를 정리해서 보여주는 기능을 넣기 위해서,
    해시태그만 뽑아서 unique 하게 보여주는 쿼리를 querydsl 로 작성
    
    그 외 서비스 코드, 컨트롤러, 관련 테스트 작성

--- src/main/java/com/fastcampus/boardproject/controller/ArticleController.java
+++ src/main/java/com/fastcampus/boardproject/controller/ArticleController.java
@@ -36,7 +36,6 @@ public class ArticleController {
 			@PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
 			ModelMap map
 	) {
-
 		Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
 		List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
 
@@ -56,6 +55,24 @@ public class ArticleController {
 		map.addAttribute("totalCount", articleService.getArticleCount());
 
 		return "articles/detail";
+	}
+
+	@GetMapping("/search-hashtag")
+	public String searchHashtag(
+		@RequestParam(required = false) String searchValue,
+		@PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+		ModelMap map
+	) {
+		Page<ArticleResponse> articles = articleService.searchArticlesViaHashtag(searchValue, pageable).map(ArticleResponse::from);
+		List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+		List<String> hashtags = articleService.getHashtags();
+
+		map.addAttribute("articles", articles);
+		map.addAttribute("hashtags", hashtags);
+		map.addAttribute("paginationBarNumbers", barNumbers);
+		map.addAttribute("searchType", SearchType.HASHTAG);
+
+		return "articles/search-hashtag";
 	}
 
 
