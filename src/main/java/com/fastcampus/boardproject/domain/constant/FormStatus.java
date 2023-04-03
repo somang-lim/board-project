f334fe42b38d23a41e9256e518203dd6b4dba57a
somang-lim <somangjuha@gmail.com>
2023-04-03 15:22:14 +0900

    #41 - 리팩토링 : FormStatus 필요하지 않는 `,` 제거
    코드 복붙할 때, 다시 한 번 확인하고
    코드 디자인 때문에 리팩토링 하는 일을 최소화 한다.

--- src/main/java/com/fastcampus/boardproject/domain/constant/FormStatus.java
+++ src/main/java/com/fastcampus/boardproject/domain/constant/FormStatus.java
@@ -4,8 +4,7 @@ import lombok.Getter;
 
 public enum FormStatus {
 	CREATE("저장", false),
-	UPDATE("수정", true),
-	;
+	UPDATE("수정", true);
 
 	@Getter private final String description;
 	@Getter private final Boolean update;
