3fa1ff9301336bf3c469f8c1d43049a8f6a7c63b
somang-lim <somangjuha@gmail.com>
2023-04-26 22:32:02 +0900

    #79 - 댓글 컨트롤러 테스트 업데이트: 대댓글 케이스 추가
    대댓글이 포함된 댓글 저장 api 의 입출력 테스트 추가

--- src/test/java/com/fastcampus/boardproject/controller/ArticleCommentControllerTest.java
+++ src/test/java/com/fastcampus/boardproject/controller/ArticleCommentControllerTest.java
@@ -81,15 +81,38 @@ class ArticleCommentControllerTest {
 
 		// When & Then
 		mvc.perform(
-			post("/comments/" + articleCommentId + "/delete")
-				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-				.content(formDataEncoder.encode(Map.of("articleId", articleId)))
-				.with(csrf())
-		)
+				post("/comments/" + articleCommentId + "/delete")
+					.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+					.content(formDataEncoder.encode(Map.of("articleId", articleId)))
+					.with(csrf())
+			)
 			.andExpect(status().is3xxRedirection())
 			.andExpect(view().name("redirect:/articles/" + articleId))
 			.andExpect(redirectedUrl("/articles/" + articleId));
 		then(articleCommentService).should().deleteArticleComment(articleCommentId, userId);
+	}
+
+	@WithUserDetails(value = "imhopeTest", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+	@DisplayName("[view][POST] 대댓글 등록 - 정상 호출")
+	@Test
+	void givenArticleCommentInfoWithParentCommentId_whenRequesting_thenSavesNewChildComment() throws Exception {
+		// Given
+		Long articleId = 1L;
+		ArticleCommentRequest request = ArticleCommentRequest.of(articleId, 1L, "test comment");
+		willDoNothing().given(articleCommentService).saveArticleComment(any(ArticleCommentDto.class));
+
+		// When & Then
+		mvc.perform(
+				post("/comments/new")
+					.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+					.content(formDataEncoder.encode(request))
+					.with(csrf())
+			)
+			.andExpect(status().is3xxRedirection())
+			.andExpect(view().name("redirect:/articles/" + articleId))
+			.andExpect(redirectedUrl("/articles/" + articleId));
+
+		then(articleCommentService).should().saveArticleComment(any(ArticleCommentDto.class));
 	}
 
 }
